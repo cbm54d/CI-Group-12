2256c25543361c817224d8c9c80de81d024f4a4b
cbm54d <cbm54d@mail.missouri.edu>
2018-12-12 23:30:40 -0600

    added goalCheck and improved display

--- RoomModel.py
+++ RoomModel.py
@@ -4,8 +4,7 @@ from matplotlib import pyplot as plt
 from matplotlib import collections as mc
 import math
 import numpy as np
-from scipy.optimize import fsolve
-#import pylab as pl
+from collections import deque
 
 class Wall:
 	def __init__(self, x1, y1, x2, y2):
@@ -18,56 +17,66 @@ class Wall:
 		self.all = (self.p1, self.p2)
 
 class StateModel:
-	def __init__(self, robotX=0, robotY=0, robotDir=0, goalX=1, goalY=1):
+	def __init__(self, robotX=0, robotY=0, robotDir=0, goalX=1, goalY=1, goalThreshold=1):
 		self.robotX = robotX
 		self.robotY = robotY
 		self.robotDir = robotDir	# Here, 0 refers to 0 degrees (what will be displayed as "up" or "north")
 		self.goalX = goalX
 		self.goalY = goalY
+		self.goalThreshold = goalThreshold
 		self.walls = []				# This list will contain wall objects (containing two coordinate pairs representing the wall's endpoints)
-		self.robotPathX = [0.1, 0.4, 0.7]		# This list will contain the x coordinates of the robot at every previous time interval (in order)
-		self.robotPathY = [0.1, 0.4, 0.7]		# Same as above but for the y coordinates
+		self.robotPathX = []		# This list will contain the x coordinates of the robot at every previous time interval (in order)
+		self.robotPathY = []		# Same as above but for the y coordinates
+		self.robotPathA = []		# Same as above but for the angle representing the direction the robot is facing at each step (in degrees)
 
 	def updateRobot(self, deltaAngle, deltaDist):		# Updates robot based on values passed from back-end
-		self.robotDir = self.robotDir + deltaAngle					# Update direction robot is facing
-		self.robotX = self.robotX + math.cos(90 - self.robotDir)	# Using updated direction, update x-coordinate of robot
-		self.robotY = self.robotY + math.sin(90 - self.robotDir)	# Same as above for y-coordinate
+		self.robotPathX.append(self.robotX)							# Records the location and angle of the robot before moving it
+		self.robotPathY.append(self.robotY)
+		self.robotPathA.append(self.robotDir)
+		self.robotDir = self.robotDir + deltaAngle										# Update direction robot is facing
+		self.robotX = self.robotX + (deltaDist * math.sin(math.radians(self.robotDir)))	# Using updated direction, update x-coordinate of robot
+		self.robotY = self.robotY + (deltaDist * math.cos(math.radians(self.robotDir)))	# Same as above for y-coordinate
+
+	def goalCheck(self):		# Checks to see if the robot has reached the goal
+		# This function will work by succeeding when the robot comes within a certain distance of the point representing the goal
+		# AT ANY POINT during its movement from the last time step to the current time step. Not exactly sure how that'll work but here goes...
+		# I'll START by having it identify if the robot ENDS UP within range of the goal at a given step
+		goalInfo = self.computeGoalInfo()
+		if goalInfo[0] < self.goalThreshold:
+			return True
+		else:
+			return False
 
 	def displayRoom(self):		# Creates a graphical display of the room's state at the time it is called
 		# I found this method for graphing multiple line segments here:
 		# https://stackoverflow.com/questions/21352580/matplotlib-plotting-numerous-disconnected-line-segments-with-different-colors
-		#lines = [[(0, 1), (1, 1)], [(2, 3), (3, 3)], [(1, 2), (1, 3)]]
 		lines = []
 		for wall in self.walls:
 			lines.append(wall.all)
 		#print lines
+		# https://stackoverflow.com/questions/36470343/how-to-draw-a-line-with-matplotlib/36479941
+
+
+		x = [self.robotX, self.robotX+0.2*math.sin(math.radians(self.robotDir))]
+		y = [self.robotY, self.robotY+0.2*math.cos(math.radians(self.robotDir))]
 		c = np.array([(1, 0, 0, 1), (0, 1, 0, 1), (0, 0, 1, 1)])
 		lc = mc.LineCollection(lines, linewidths=2)
+		goalZone = plt.Circle((self.goalX, self.goalY), self.goalThreshold, color='g')
 		fig, ax = plt.subplots()
+		ax.add_artist(goalZone)
+		ax.plot(x, y)
 		ax.add_collection(lc)
-
-		ax.scatter(self.robotPathX, self.robotPathY, color='k')
-		ax.scatter(self.robotX, self.robotY, color='g')
-		ax.scatter(self.goalX, self.goalY, color='r')
+		
+		ax.scatter(self.robotPathX, self.robotPathY, color='grey')
+		ax.scatter(self.robotX, self.robotY, color='blue')
+		ax.scatter(self.goalX, self.goalY, color='green')
 
 		#ax.autoscale()
 		ax.margins(0.1)
+		ax.axis('scaled')
 
 		plt.show()
 
-	# And now the fun REALLY begins...
-	def computeObjInfo(self):
-
-		minDist = 1000
-		minAngle = 0
-
-		for wall in self.walls:
-
-			dist = None
-			angle = None
-			perpSlope = None
-			slope = None
-			robotPerpToWall = False
 
 	# Okay, let's take a moment to think about how this thing should work GIVEN THAT SOME LINES CAN BE VERTICAL.
 	# Vertical lines have an UNDEFINED slope (involves a division by zero), so they cause problems for the program.
@@ -90,10 +99,24 @@ class StateModel:
 	#		a perpendicular with the wall, the pythagorean theorem is all that is required to calculate the distance. However, when
 	#		computing the ANGLE of the nearest point to the robot, a tan(_/0) is used, so this case must still be accounted for.
 
+	# And now the fun REALLY begins...
+	def computeObjInfo(self):
+
+		minDist = 1000
+		minAngle = 0
+
+		for wall in self.walls:
+
+			dist = None
+			angle = None
+			perpSlope = None
+			slope = None
+			robotPerpToWall = False
+
 			# Account for cases which would otherwise involve dividing by zero
 			# CASE 1s: wall with a vertical (undefined) slope
 			if wall.x1 == wall.x2:
-				print "~~! CASE 1s: wall with vertical slope !~~"
+				#print "~~! CASE 1s: wall with vertical slope !~~"
 				if self.robotY >= wall.y1 and self.robotY <= wall.y2:
 					robotPerpToWall = True
 				if self.robotY <= wall.y1 and self.robotY >= wall.y2:
@@ -101,7 +124,7 @@ class StateModel:
 
 			# CASE 2s: wall with a horizontal slope & vertical (undefined) distance vector
 			elif wall.y1 == wall.y2:
-				print "~~! CASE 2s: wall with horizontal slope !~~"
+				#print "~~! CASE 2s: wall with horizontal slope !~~"
 				if self.robotX >= wall.x1 and self.robotX <= wall.x2:
 					robotPerpToWall = True
 				if self.robotX <= wall.x1 and self.robotX >= wall.x2:
@@ -116,7 +139,7 @@ class StateModel:
 				xBound1 = (self.robotY - perpYIntercept1) / perpSlope
 				perpYIntercept2 = wall.y2 - (perpSlope * wall.x2)
 				xBound2 = (self.robotY - perpYIntercept2) / perpSlope
-				print("xBound1 = {0}       xBound2 = {1}".format(xBound1, xBound2))
+				#print("xBound1 = {0}       xBound2 = {1}".format(xBound1, xBound2))
 				if self.robotX >= xBound1 and self.robotX <= xBound2:
 					robotPerpToWall = True
 				elif self.robotX <= xBound1 and self.robotX >= xBound2:
@@ -124,7 +147,7 @@ class StateModel:
 
 			# Calculate distance from robot to nearest point along wall
 			if robotPerpToWall:											# Case 1: robot is on a perpendicular with the wall
-				print("Wall is perpendicular to the robot")				# CASE 1s: wall with a vertical (undefined) slope
+				#print("Wall is perpendicular to the robot")				# CASE 1s: wall with a vertical (undefined) slope
 				if wall.x1 == wall.x2: 
 					dist = abs(self.robotX - wall.x1)
 					angleFromZero = 90 if self.robotX > wall.x1 else 270
@@ -135,25 +158,13 @@ class StateModel:
 
 				else:
 					perpYInterceptR = self.robotY - (perpSlope * self.robotX)
-					print("perpSlope = {0}".format(perpSlope))
-					print("slope = {0}".format(slope))
-					print("perpYInterceptR = {0}".format(perpYInterceptR))
-					print("yIntercept = {0}".format(yIntercept))
-
-					#nearestPoint = get_intersect(wall.p1, wall.p2, (self.robotX, self.robotY), (0, perpYInterceptR))
 					nearestPoint = line_intersect(wall.p1, (self.robotX, self.robotY), slope, perpSlope)
-					print("NEAREST POINT = {0}".format(nearestPoint))
-
-					# a = np.array([[-perpSlope, 1], [slope, 1]])
-					# b = np.array([perpYInterceptR, yIntercept])
-					# nearestPoint = np.linalg.solve(a, b)			# https://docs.scipy.org/doc/numpy-1.13.0/reference/generated/numpy.linalg.solve.html
 					dist = math.sqrt((self.robotX - nearestPoint[0]) ** 2 + (self.robotY - nearestPoint[1]) ** 2)
-					print("angle = acos({0})".format(abs(self.robotY - nearestPoint[1])/dist))
+					#print("angle = acos({0})".format(abs(self.robotY - nearestPoint[1])/dist))
 					angleFromZero = math.degrees(math.acos((nearestPoint[1] - self.robotY)/dist))
 
 			else:														# Case 2: robot is not on perpendicular with the wall
-				print("Wall is not perpendicular to the robot")
-
+				#print("Wall is not perpendicular to the robot")
 				dist1 = math.sqrt((self.robotX - wall.x1) ** 2 + (self.robotY - wall.y1) ** 2)
 				dist2 = math.sqrt((self.robotX - wall.x2) ** 2 + (self.robotY - wall.y2) ** 2)
 				dist = min([dist1, dist2])
@@ -161,19 +172,26 @@ class StateModel:
 					p = (wall.x1, wall.y1)
 				else:
 					p = (wall.x2, wall.y2)
+
 				if wall.x1 == self.robotX or wall.x2 == self.robotX:	# CASE 3s: vertical distance vector to wall endpoint
 					angleFromZero = 180 if self.robotY > wall.y1 else 0
 				else:
 					angleFromZero = math.degrees(math.atan((self.robotX - p[0]) / (self.robotY - p[1])))
 
 			angle = angleFromZero - self.robotDir
-			print( "for some wall: dist = {0} & angle = {1}".format(dist, angle))
+			#print( "for some wall: dist = {0} & angle = {1}".format(dist, angle))
 			if dist < minDist:
-				print("since {0} < {1}, new minDist is {0}".format(dist, minDist))
 				minDist = dist
 				minAngle = angle
 
 		return (minDist, minAngle)
+
+	def computeGoalInfo(self):
+		dx = self.goalX - self.robotX
+		dy = self.goalY - self.robotY
+		goalDist = math.sqrt(dx**2 + dy**2)
+		goalAngle = math.degrees(math.atan(dy/dx))
+		return (goalDist, goalAngle)
 
 # The function below comes from https://stackoverflow.com/questions/3252194/numpy-and-line-intersections
 def line_intersect(p0, p1, m0=None, m1=None, q0=None, q1=None):
@@ -208,11 +226,8 @@ def line_intersect(p0, p1, m0=None, m1=None, q0=None, q1=None):
 		lhs1 = [-m1, 1]
 		rhs1 = p1[1] - m1 * p1[0]
 
-	a = np.array([lhs0, 
-					lhs1])
-
-	b = np.array([rhs0, 
-					rhs1])
+	a = np.array([lhs0, lhs1])
+	b = np.array([rhs0, rhs1])
 	try:
 		px = np.linalg.solve(a, b)
 	except:
@@ -220,12 +235,10 @@ def line_intersect(p0, p1, m0=None, m1=None, q0=None, q1=None):
 
 	return px
 
-def main():
-	print("Now starting main...")
+def initialize():
 	print("Initializing StateModel object...")
-	# StateModel(robotX, robotY, robotAngle, goalX, goalY)
-	stateModel = StateModel(2, 2, 45, 4, 4)
-
+	# StateModel(robotX, robotY, robotAngle, goalX, goalY, goalThreshold)
+	stateModel = StateModel(1, 1, 45, 4, 4, 0.25)
 	print("Generating walls...")
 	# The walls listed below define the boundaries of the room, which is currently expressed as a 5x5 grid.
 	boundaries = [Wall(0,0,5,0), Wall(5,0,5,5), Wall(5,5,0,5), Wall(0,5,0,0)]
@@ -234,16 +247,48 @@ def main():
 
 	# For the time being, extra walls must be hard-coded here.
 	# To add a wall, include Wall(x1, x2, y1, y2) in the list assigned to the walls variable.
-	walls = [Wall(3,3,3,4)]
-		   #[Wall(2,3,3,2), Wall(2,1,3,2), Wall(4,2,5,3)]
+	walls = [Wall(2,3,3,2)]
+		   #[Wall(2,3,3,2), Wall(2,1,3,2), Wall(4,2,5,3), Wall(3,3,3,4)]
 		   #[Wall(1,2,2,3), Wall(3,2,3,3), Wall(3,3,5,3)]
-	
 	for wall in walls:
 		stateModel.walls.append(wall)
 
+	return stateModel
+
+def main():
+
+	# A lot of the below code is designed for testing purposes.
+	# The contents of main() should be manipulated to suit the needs of the one utilizing the program at any given time.
+
+	print("Now starting main...")
+	stateModel = initialize()
+	stateModel.displayRoom()
 	(dist, angle) = stateModel.computeObjInfo()
 	print("from main:\ndist = {0}\nangle = {1}".format(dist, angle))
+	moveList = [
+			[0,0.7],   [0,0.5],   [-30,0.45],
+			[-21,0.4], [-5,0.3],  [19,0.2],
+			[35,0.5],  [30,0.57], [4,0.7], 
+			[0,0.4]]
+
+	goalReached = False
+	noMoreMoves = False
+	moveQueue = deque(moveList)
+	while not goalReached and not noMoreMoves:
+		move = moveQueue.popleft()
+		stateModel.updateRobot(move[0], move[1])
+		goalReached = stateModel.goalCheck()
+		noMoreMoves = len(moveQueue) == 0
+
 	stateModel.displayRoom()
+	if goalReached:
+		print "GOAL REACHED!!!"
+	elif noMoreMoves:
+		print "The test has run out of moves without reaching the goal."
+	else:
+		print "I don't think this should happen..."
+
+
 
 main()
 
